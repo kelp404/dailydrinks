f37c193cde32438c199ceb0780edac5afbb0417d
Kelp <kelp@phate.org>
2019-04-26 08:26:34 +0800

    Revert "Removed why-did-you-update."
    This reverts commit 18fc7c8a10a00d1c8d00c7590c536c8cbd200eb7.

--- src/web.js
+++ src/web.js
@@ -18,6 +18,10 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const router = require('./router');
 
+if (process.env.NODE_ENV !== 'production') {
+  const {whyDidYouUpdate} = require('why-did-you-update');
+  whyDidYouUpdate(React);
+}
 nprogress.configure({showSpinner: false});
 
 router.listen('ChangeStart', () => {
